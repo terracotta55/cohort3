a90b802edeaaa73ce81f9a8bc01e2982645bf495
Dalton Okechukwu <Dalton@Dalton-Mac-Air.local>
2019-10-11 10:11:28 -0600

    added new daily

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -13,25 +13,33 @@
 */
 
 // Write the function after this comment ---
-
-function assertEquals(a, b) {
-  if (a === b) {
-    console.log(`*** the two values are the same`);
-    return true;
-  } else {
-    console.log(`*** the two values are NOT the same
+const functions = {
+  assertEquals: (a, b) => {
+    if (a === b) {
+      console.log(`*** the two values are the same`);
+      return true;
+    } else {
+      console.log(`*** the two values are NOT the same
       p1--> ${a} (parameter 1)
       p2--> ${b} (parameter 2)`);
-    return false;
-  }
-}
+      return false;
+    }
+  },
 
-export { assertEquals };
+  makeEmailArr: arr => {
+    let first = arr[0].toLowerCase();
+    let last = arr[1].toLowerCase();
+    return `${first}.${last}@evolveu.ca`;
+  }
+};
+``;
+
+export default functions;
 // and before this comment ---
 
-assertEquals("a", "b");
-assertEquals("a", "a");
-assertEquals(1, 2);
-assertEquals(2, 2);
-assertEquals("2", 2);
-assertEquals("This value", "This value");
+// assertEquals("a", "b");
+// assertEquals("a", "a");
+// assertEquals(1, 2);
+// assertEquals(2, 2);
+// assertEquals("2", 2);
+// assertEquals("This value", "This value");
