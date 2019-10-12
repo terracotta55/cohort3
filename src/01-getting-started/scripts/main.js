17fc4b5540abfd46b81ebcc5517f5cad1634f945
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-11 22:03:04 -0600

    fixed main.js

--- src/01-getting-started/scripts/main.js
+++ src/01-getting-started/scripts/main.js
@@ -1,8 +1,9 @@
 import functions from "./functions.js";
-
-// add event listeners
+/*
+add event listeners
 let idNumber = document.getElementById("idNumber");
 let idNumberSize = document.getElementById("idNumberSize");
+*/
 idNumber.addEventListener("change", () => {
   idNumberSize.textContent = functions.size(idNumber.value);
 });
