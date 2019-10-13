9e1dcede27257878152c35226c79e9aa8c14b36d
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-13 00:15:40 -0600

    added test for taxes

--- src/01-getting-started/scripts/functions.test.js
+++ src/01-getting-started/scripts/functions.test.js
@@ -2,23 +2,32 @@ import functions from "./functions";
 import calculator from "./calc-func";
 
 test("testing size function", () => {
-  expect(functions.size(-1)).toBe("negative"); // Consider the edge cases
-  expect(functions.size(0)).toBe("small");
-  expect(functions.size(10)).toBe("medium");
-  expect(functions.size(15)).toBe("medium");
-  expect(functions.size(20)).toBe("large");
-  expect(functions.size(101)).toBe("extra large");
-  expect(functions.size(2000000)).toBe("extra large");
+	expect(functions.size(-1)).toBe("negative"); // Consider the edge cases
+	expect(functions.size(0)).toBe("small");
+	expect(functions.size(10)).toBe("medium");
+	expect(functions.size(15)).toBe("medium");
+	expect(functions.size(20)).toBe("large");
+	expect(functions.size(101)).toBe("extra large");
+	expect(functions.size(2000000)).toBe("extra large");
 });
 
 test("testing add function", () => {
-  expect(functions.add(1, 2)).toBe(3);
-  expect(functions.add(101, 202)).toBe(303);
+	expect(functions.add(1, 2)).toBe(3);
+	expect(functions.add(101, 202)).toBe(303);
 });
 
 test("testing calculator function", () => {
-  expect(calculator.add(30, 10)).toBe(40);
-  expect(calculator.sub(30, 10)).toBe(20);
-  expect(calculator.mul(10, 5)).toBe(50);
-  expect(calculator.dvd(10, 5)).toBe(2);
+	expect(calculator.add(30, 10)).toBe(40);
+	expect(calculator.sub(30, 10)).toBe(20);
+	expect(calculator.mul(10, 5)).toBe(50);
+	expect(calculator.dvd(10, 5)).toBe(2);
+});
+
+test("testing tax calc", () => {
+	expect(functions.taxes(1)).toEqual(0.15);
+	expect(functions.taxes(2)).toEqual(0.3);
+	expect(functions.taxes(50000)).toEqual(7630.35);
+	expect(functions.taxes(100000)).toEqual(18141.1);
+	expect(functions.taxes(150000)).toEqual(31211.1);
+	expect(functions.taxes(250000)).toEqual(61796.25);
 });
