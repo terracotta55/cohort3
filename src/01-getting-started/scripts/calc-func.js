4e398c0e64461f232395e6d293384040835e6812
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-13 00:14:35 -0600

    added tax calculator

--- src/01-getting-started/scripts/calc-func.js
+++ src/01-getting-started/scripts/calc-func.js
@@ -2,81 +2,54 @@ let inputOne = document.querySelector("#calc-input-1");
 let inputTwo = document.querySelector("#calc-input-2");
 
 const calculator = {
-  add: (num1, num2) => {
-    return num1 + num2;
-  },
-  sub: (num1, num2) => {
-    return num1 - num2;
-  },
-  mul: (num1, num2) => {
-    return num1 * num2;
-  },
-  dvd: (num1, num2) => {
-    return num1 / num2;
-  }
+	add: (num1, num2) => {
+		return num1 + num2;
+	},
+	sub: (num1, num2) => {
+		return num1 - num2;
+	},
+	mul: (num1, num2) => {
+		return num1 * num2;
+	},
+	dvd: (num1, num2) => {
+		return num1 / num2;
+	}
 };
 
 function onAddButtonClicked() {
-  let addResult = calculator.add(
-    Number(inputOne.value),
-    Number(inputTwo.value)
-  );
-  document.getElementById(
-    "calc-result"
-  ).innerHTML = `the sum of ${inputOne.value} and ${inputTwo.value} is ${addResult}`;
-  inputOne.value = "";
-  inputTwo.value = "";
+	let addResult = calculator.add(Number(inputOne.value), Number(inputTwo.value));
+	document.getElementById("calc-result").innerHTML = `the sum of ${inputOne.value} and ${inputTwo.value} is ${addResult}`;
+	inputOne.value = "";
+	inputTwo.value = "";
 }
 function onSubButtonClicked() {
-  let subResult = calculator.sub(
-    Number(inputOne.value),
-    Number(inputTwo.value)
-  );
-  document.getElementById(
-    "calc-result"
-  ).innerHTML = `the difference between ${inputOne.value} and ${inputTwo.value} is ${subResult}`;
-  inputOne.value = "";
-  inputTwo.value = "";
+	let subResult = calculator.sub(Number(inputOne.value), Number(inputTwo.value));
+	document.getElementById("calc-result").innerHTML = `the difference between ${inputOne.value} and ${inputTwo.value} is ${subResult}`;
+	inputOne.value = "";
+	inputTwo.value = "";
 }
 function onMulButtonClicked() {
-  let mulResult = calculator.mul(
-    Number(inputOne.value),
-    Number(inputTwo.value)
-  );
-  document.getElementById(
-    "calc-result"
-  ).innerHTML = `the product of ${inputOne.value} and ${inputTwo.value} is ${mulResult}`;
-  inputOne.value = "";
-  inputTwo.value = "";
+	let mulResult = calculator.mul(Number(inputOne.value), Number(inputTwo.value));
+	document.getElementById("calc-result").innerHTML = `the product of ${inputOne.value} and ${inputTwo.value} is ${mulResult}`;
+	inputOne.value = "";
+	inputTwo.value = "";
 }
 function onDvdButtonClicked() {
-  let dvdResult = calculator.dvd(
-    Number(inputOne.value),
-    Number(inputTwo.value)
-  );
-  document.getElementById(
-    "calc-result"
-  ).innerHTML = `the quotient of ${inputOne.value} and ${inputTwo.value} is ${dvdResult}`;
-  inputOne.value = "";
-  inputTwo.value = "";
+	let dvdResult = calculator.dvd(Number(inputOne.value), Number(inputTwo.value));
+	document.getElementById("calc-result").innerHTML = `the quotient of ${inputOne.value} and ${inputTwo.value} is ${dvdResult}`;
+	inputOne.value = "";
+	inputTwo.value = "";
 }
 
-// **** comment this out to run test
-document
-  .getElementById("btn-add")
-  .addEventListener("click", onAddButtonClicked);
+/*
+ **** comment this out to run test
+ */
+document.getElementById("btn-add").addEventListener("click", onAddButtonClicked);
+document.getElementById("btn-sub").addEventListener("click", onSubButtonClicked);
+document.getElementById("btn-mul").addEventListener("click", onMulButtonClicked);
+document.getElementById("btn-dvd").addEventListener("click", onDvdButtonClicked);
 
-document
-  .getElementById("btn-sub")
-  .addEventListener("click", onSubButtonClicked);
-
-document
-  .getElementById("btn-mul")
-  .addEventListener("click", onMulButtonClicked);
-
-document
-  .getElementById("btn-dvd")
-  .addEventListener("click", onDvdButtonClicked);
-
-// **** comment this out to run javascript
+/*
+ **** comment this out to run javascript
+ */
 // export default calculator;
--- src/01-getting-started/scripts/functions.js
+++ src/01-getting-started/scripts/functions.js
@@ -1,19 +1,58 @@
 const functions = {
-  size: num => {
-    if (num < 0) return "negative";
-    if (num < 10) return "small";
-    if (num < 20) return "medium";
-    if (num <= 100) return "large";
-    return "extra large";
-  },
+	size: num => {
+		if (num < 0) return "negative";
+		if (num < 10) return "small";
+		if (num < 20) return "medium";
+		if (num <= 100) return "large";
+		return "extra large";
+	},
 
-  add: (num1, num2) => {
-    return num1 + num2;
-  },
+	add: (num1, num2) => {
+		return num1 + num2;
+	},
 
-  subtract: (num1, num2) => {
-    return num1 - num2;
-  }
+	subtract: (num1, num2) => {
+		return num1 - num2;
+	},
+
+	taxes: income => {
+		let tax = 0;
+		const taxBracket_01 = 47630,
+			taxBracket_02 = 95259,
+			taxBracket_03 = 147667,
+			taxBracket_04 = 210371;
+		if (income <= taxBracket_01) {
+			tax = (income * 0.15).toFixed(2);
+		} else if (income > taxBracket_01 && income <= taxBracket_02) {
+			tax = (taxBracket_01 * 0.15 + (income - taxBracket_01) * 0.205).toFixed(2);
+		} else if (income > taxBracket_02 && income <= taxBracket_03) {
+			tax = (taxBracket_01 * 0.15 + (taxBracket_02 - taxBracket_01) * 0.205 + (income - taxBracket_02) * 0.26).toFixed(2);
+		} else if (income > taxBracket_03 && income <= taxBracket_04) {
+			tax = (
+				taxBracket_01 * 0.15 +
+				(taxBracket_02 - taxBracket_01) * 0.205 +
+				(taxBracket_03 - taxBracket_02) * 0.26 +
+				(income - taxBracket_03) * 0.29
+			).toFixed(2);
+		} else if (income > taxBracket_04) {
+			tax = (
+				taxBracket_01 * 0.15 +
+				(taxBracket_02 - taxBracket_01) * 0.205 +
+				(taxBracket_03 - taxBracket_02) * 0.26 +
+				(taxBracket_04 - taxBracket_03) * 0.29 +
+				(income - taxBracket_04) * 0.33
+			).toFixed(2);
+		}
+		return Number(tax);
+	},
+
+	onTaxButtonClicked: () => {
+		let taxInput = document.querySelector("#tax-input");
+		let taxResult = functions.taxes(Number(taxInput.value));
+		document.getElementById("tax-result").innerHTML = `your 2019 taxes would be $${taxResult}`;
+		taxInput.value = "";
+	}
 };
 
+document.getElementById("btn-tax").addEventListener("click", functions.onTaxButtonClicked);
 export default functions;
