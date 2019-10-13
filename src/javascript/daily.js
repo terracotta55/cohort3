d0f203773b50a61365915b59944ae4a91a2eb6ec
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-13 00:19:18 -0600

    added email object fn

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -14,23 +14,29 @@
 
 // Write the function after this comment ---
 const functions = {
-  assertEquals: (a, b) => {
-    if (a === b) {
-      console.log(`*** the two values are the same`);
-      return true;
-    } else {
-      console.log(`*** the two values are NOT the same
+	assertEquals: (a, b) => {
+		if (a === b) {
+			console.log(`*** the two values are the same`);
+			return true;
+		} else {
+			console.log(`*** the two values are NOT the same
       p1--> ${a} (parameter 1)
       p2--> ${b} (parameter 2)`);
-      return false;
-    }
-  },
+			return false;
+		}
+	},
 
-  makeEmailArr: arr => {
-    let first = arr[0].toLowerCase();
-    let last = arr[1].toLowerCase();
-    return `${first}.${last}@evolveu.ca`;
-  }
+	makeEmailArr: arr => {
+		let first = arr[0].toLowerCase();
+		let last = arr[1].toLowerCase();
+		return `${first}.${last}@evolveu.ca`;
+	},
+
+	makeEmailObj: obj => {
+		let first = obj.fname.toLowerCase();
+		let last = obj.lname.toLowerCase();
+		return `${first}.${last}@evolveu.ca`;
+	}
 };
 
 export default functions;
