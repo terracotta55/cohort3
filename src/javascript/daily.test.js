474e824d61314d51b77c6c0126733571de7d6c58
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-13 00:19:58 -0600

    tested email object fn

--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -1,25 +1,28 @@
 import functions from "./daily";
 
 test("Welcome text", () => {
-  console.log("Hello World");
+	console.log("Hello World");
 });
 
 test("Comparing numbers", () => {
-  expect(functions.assertEquals(3, 3)).toBe(true);
-  expect(functions.assertEquals("five", "five")).toBe(true);
-  expect(functions.assertEquals("20", 20)).toBe(false);
-  expect(functions.assertEquals("five", 5)).toBe(false);
+	expect(functions.assertEquals(3, 3)).toBe(true);
+	expect(functions.assertEquals("five", "five")).toBe(true);
+	expect(functions.assertEquals("20", 20)).toBe(false);
+	expect(functions.assertEquals("five", 5)).toBe(false);
 });
 
 test("email builder from an array", () => {
-  const name = ["first", "last"];
-  expect(functions.makeEmailArr(name)).toEqual("first.last@evolveu.ca");
-  expect(functions.makeEmailArr(["First", "Last"])).toEqual(
-    "first.last@evolveu.ca"
-  );
-  expect(functions.makeEmailArr(["Bill", "Smith"])).toEqual(
-    "bill.smith@evolveu.ca"
-  );
+	const name = ["first", "last"];
+	expect(functions.makeEmailArr(name)).toEqual("first.last@evolveu.ca");
+	expect(functions.makeEmailArr(["First", "Last"])).toEqual("first.last@evolveu.ca");
+	expect(functions.makeEmailArr(["Bill", "Smith"])).toEqual("bill.smith@evolveu.ca");
+});
+
+test("email builder from an object / map", () => {
+	const name = { fname: "first", lname: "last" };
+	expect(functions.makeEmailObj(name)).toEqual("first.last@evolveu.ca");
+	expect(functions.makeEmailObj({ fname: "First", lname: "Last" })).toEqual("first.last@evolveu.ca");
+	expect(functions.makeEmailObj({ fname: "Bill", lname: "Smith" })).toEqual("bill.smith@evolveu.ca");
 });
 
 // test("Check the sizes", () => {
