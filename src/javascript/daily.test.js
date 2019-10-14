0af71c4c51d150e235966d3ec4c37cabf41e1f58
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-13 21:43:39 -0600

    housekeeping - minor comments

--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -25,17 +25,19 @@ test("email builder from an object / map", () => {
 	expect(functions.makeEmailObj({ fname: "Bill", lname: "Smith" })).toEqual("bill.smith@evolveu.ca");
 });
 
-// test("Check the sizes", () => {
-//   expect(functions.size(-1)).toBe("negative"); // Consider the edge cases
-//   expect(functions.size(0)).toBe("small");
-//   expect(functions.size(10)).toBe("medium");
-//   expect(functions.size(15)).toBe("medium");
-//   expect(functions.size(20)).toBe("large");
-//   expect(functions.size(101)).toBe("extra large");
-//   expect(functions.size(2000000)).toBe("extra large");
-// });
+/*
+test("Check the sizes", () => {
+  expect(functions.size(-1)).toBe("negative"); // Consider the edge cases
+  expect(functions.size(0)).toBe("small");
+  expect(functions.size(10)).toBe("medium");
+  expect(functions.size(15)).toBe("medium");
+  expect(functions.size(20)).toBe("large");
+  expect(functions.size(101)).toBe("extra large");
+  expect(functions.size(2000000)).toBe("extra large");
+});
 
-// test("Does that add function work?", () => {
-//   expect(functions.add(1, 2)).toBe(3);
-//   expect(functions.add(101, 202)).toBe(303);
-// });
+test("Does that add function work?", () => {
+  expect(functions.add(1, 2)).toBe(3);
+  expect(functions.add(101, 202)).toBe(303);
+});
+*/
