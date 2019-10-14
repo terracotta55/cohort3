6848fd0977c5fe22cbddff46109e9dde167f0d83
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-13 19:37:08 -0600

    minor comments update

--- src/01-getting-started/scripts/main.js
+++ src/01-getting-started/scripts/main.js
@@ -1,9 +1,11 @@
 import functions from "./functions.js";
+// import calculator from "./calc-func.js";
+
 /*
 add event listeners
 let idNumber = document.getElementById("idNumber");
 let idNumberSize = document.getElementById("idNumberSize");
 */
 idNumber.addEventListener("change", () => {
-  idNumberSize.textContent = functions.size(idNumber.value);
+	idNumberSize.textContent = functions.size(idNumber.value);
 });
