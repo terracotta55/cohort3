0c3da5986975b1e897c0797765ae9083d7dd2908
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-13 21:41:19 -0600

    housekeeping on comments

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,18 +1,3 @@
-/*	
-	Write the function that will create this output:
-
-*** the two values are not the same:
-    p1--> a
-    p2--> b
-*** the two values are not the same:
-    p1--> 1
-    p2--> 2
-*** the two values are not the same:
-    p1--> 2
-    p2--> 2
-*/
-
-// Write the function after this comment ---
 const functions = {
 	assertEquals: (a, b) => {
 		if (a === b) {
@@ -40,11 +25,11 @@ const functions = {
 };
 
 export default functions;
-// and before this comment ---
-
-// assertEquals("a", "b");
-// assertEquals("a", "a");
-// assertEquals(1, 2);
-// assertEquals(2, 2);
-// assertEquals("2", 2);
-// assertEquals("This value", "This value");
+/*
+assertEquals("a", "b");
+assertEquals("a", "a");
+assertEquals(1, 2);
+assertEquals(2, 2);
+assertEquals("2", 2);
+assertEquals("This value", "This value");
+*/
