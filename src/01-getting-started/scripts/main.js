efd267b31eb51664828d045e6c5be25fbe67899a
Dalton Okechukwu <Dalton@Dalton-Mac-Air.local>
2019-10-15 16:30:24 -0600

    moved event listeners to main.js

--- src/01-getting-started/scripts/main.js
+++ src/01-getting-started/scripts/main.js
@@ -1,11 +1,23 @@
 import functions from "./functions.js";
-// import calculator from "./calc-func.js";
 
 /*
 add event listeners
-let idNumber = document.getElementById("idNumber");
-let idNumberSize = document.getElementById("idNumberSize");
 */
+
 idNumber.addEventListener("change", () => {
 	idNumberSize.textContent = functions.size(idNumber.value);
 });
+
+document.getElementById("btn-add").addEventListener("click", functions.onAddButtonClicked);
+document.getElementById("btn-sub").addEventListener("click", functions.onSubButtonClicked);
+document.getElementById("btn-mul").addEventListener("click", functions.onMulButtonClicked);
+document.getElementById("btn-dvd").addEventListener("click", functions.onDvdButtonClicked);
+document.getElementById("btn-calc-clear").addEventListener("click", functions.onClearCalcClicked);
+
+document.getElementById("btn-tax").addEventListener("click", functions.onTaxButtonClicked);
+document.getElementById("btn-arr-add").addEventListener("click", functions.onAddToArrayClicked);
+document.getElementById("btn-arr-show").addEventListener("click", functions.onShowArrayClicked);
+document.getElementById("btn-arr-total").addEventListener("click", functions.onTotalArrayClicked);
+document.getElementById("btn-arr-clear").addEventListener("click", functions.onClearArrayClicked);
+
+document.getElementById("btn-obj-lookup").addEventListener("click", functions.onObjLookupClicked);
