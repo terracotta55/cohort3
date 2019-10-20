775d4e816e861481db07ea0bd2eadf5447c1d01f
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-20 14:10:50 -0600

    used event delegation for listening

--- src/02-dom/scripts/cards_main.js
+++ src/02-dom/scripts/cards_main.js
@@ -1,39 +1,33 @@
 import functionsCards from "./cards.js";
-
-let btnAddCard = document.getElementById("btn-top-add");
-// let btnDel = document.getElementById("btn-del-1");
-// let cardsList = document.getElementsByClassName("btn-card-del");
 /*
-add event listeners below
-*/
-btnAddCard.addEventListener("click", functionsCards.onAddCardClicked);
-// btnDel.addEventListener("click", functionsCards.onDelBtnClicked);
-/*
-// add event listener to a class (see cardList)
+let btnDel = document.getElementById("btn-del-1");
+let cardsList = document.getElementsByClassName("btn-card-del");
+btnDel.addEventListener("click", functionsCards.onDelBtnClicked);
+// adding event listener to a class (see cardList)
 Array.from(cardsList).forEach(function(e) {
 	e.addEventListener("click", functionsCards.onDelBtnClicked);
 });
 */
+let btnAddCard = document.getElementById("btn-top-add");
+btnAddCard.addEventListener("click", functionsCards.onAddCardClicked);
 left.addEventListener("click", function(e) {
 	// this is called event delegation
-	// e.target is the clicked element
-	// if it was an item with class name btn-card-del
 	if (e.target && e.target.className == "btn-card-del") {
-		// button element found! display console message and carry out remove function
-		console.log("delete card clicked");
+		// e.target is the clicked element
+		console.log("event listener: delete"); // button with class name found!
 		functionsCards.removeCurrCard(e);
 	}
 });
 
 left.addEventListener("click", function(e) {
 	if (e.target && e.target.className == "btn-card-before") {
-		console.log("card before clicked");
+		console.log("event listener: before");
 		functionsCards.addBeforeCard(e);
 	}
 });
 left.addEventListener("click", function(e) {
 	if (e.target && e.target.className == "btn-card-after") {
-		console.log("card after clicked");
+		console.log("event listener: after");
 		functionsCards.addAfterCard(e);
 	}
 });
