c89ca47f46ee9baa50b36ea3e76031b88f0121f1
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-20 17:42:06 -0600

    changed test parameters

--- src/02-dom/scripts/functions.test.js
+++ src/02-dom/scripts/functions.test.js
@@ -1,17 +1,17 @@
 import functions from "./functions.js";
 import functionsCards from "./cards.js";
 
-test("testing addition in functions.js", () => {
-	expect(functions.add(10, 20)).toBe(30);
+test("testing add fn in functions.js", () => {
+	expect(functions.add(60, 40)).toBe(100);
 	expect(functions.add(101, 202)).toBe(303);
 });
 
-test("testing subtraction in cards.js", () => {
-	expect(functionsCards.sub(50, 23)).toBe(27);
-	expect(functionsCards.sub(300, 202)).toBe(98);
+test("testing sub fn in cards.js", () => {
+	expect(functionsCards.sub(500, 430)).toBe(70);
+	expect(functionsCards.sub(300, 205)).toBe(95);
 });
 
-test("testing add new card in cards.js", () => {
+test("testing add new card fn in cards.js", () => {
 	document.body.innerHTML = `<div class="card-one" id="box-one"><p class="card-text-content">Card 0</p><button class="btn-card-before">Add Card Before</button><button class="btn-card-after">Add Card After</button><button class="btn-card-del">Delete Card</button></div>`;
 
 	let newCard = document.querySelector(".card-one");
