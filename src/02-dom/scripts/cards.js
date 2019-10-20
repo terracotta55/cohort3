c51ba1b4c01844a53c67ee322ac48be5ea485abc
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-19 21:20:34 -0600

    card add and card delete (1st card) functionality

--- src/02-dom/scripts/cards.js
+++ src/02-dom/scripts/cards.js
@@ -1,32 +1,46 @@
 let left = document.getElementById("left");
 // let btnAddCard = document.getElementById("btn-top-add");
-// let btnDel = document.getElementById("btn-del");
+// let btnDel = document.getElementsByClassName("btn-card-del");
 // let btnAdd = document.getElementById("btn-add");
 // let btnAfter = document.getElementById("btn-after");
+let cardsList = document.getElementsByClassName("btn-card-del");
+let cardIndex = 2;
+// let origCard = document.getElementsByClassName("card")[cardIndex - 2];
 
 const functionsCards = {
 	addNewCard: () => {
-		let origCard = document.getElementById("box-1");
+		let origCard = document.getElementsByClassName("card")[0];
 		let dupCard = origCard.cloneNode(true);
-		let left = document.getElementById("left");
-		console.log(origCard);
+
+		console.log(dupCard);
 		left.appendChild(dupCard);
+		//c change id and text content after appending clone
+		document.getElementsByClassName("card")[cardIndex - 1].setAttribute("id", `box-${cardIndex}`);
+		document.getElementsByClassName("btn-card-del")[cardIndex - 1].setAttribute("id", `btn-del-${cardIndex}`);
+		document.getElementsByClassName("card-text-content")[cardIndex - 1].textContent = `Card-${cardIndex}`;
+		cardIndex++;
+
 		return true;
 	},
 	onAddCardClicked: () => {
 		console.log("add clicked");
 		functionsCards.addNewCard();
 	},
-	removeCurrCard: e => {
+	removeCurrCard: () => {
+		/*
+		e.preventDefault();
 		if (e.target.tagName === "button") {
 			left.remove(e.target.parent);
-			// e.target.parentNode.remove();
 		}
+		*/
+		let origCard = document.getElementsByClassName("card")[0];
+		origCard.parentNode.removeChild(origCard);
+
 		return true;
 	},
-	onDelBtnClicked: e => {
-		console.log("del clicked");
-		functionsCards.removeCurrCard(e);
+	onDelBtnClicked: () => {
+		console.log("delete clicked");
+		functionsCards.removeCurrCard();
 		return true;
 	}
 };
--- src/02-dom/scripts/cards_main.js
+++ src/02-dom/scripts/cards_main.js
@@ -1,9 +1,25 @@
 import functionsCards from "./cards.js";
 
 let btnAddCard = document.getElementById("btn-top-add");
-let btnDel = document.getElementById("btn-del");
+// let btnDel = document.getElementById("btn-del-1");
+// let cardsList = document.getElementsByClassName("btn-card-del");
 /*
 add event listeners below
 */
 btnAddCard.addEventListener("click", functionsCards.onAddCardClicked);
-btnDel.addEventListener("click", functionsCards.onDelBtnClicked);
+// btnDel.addEventListener("click", functionsCards.onDelBtnClicked);
+/*
+// add event listener to a class (see cardList)
+Array.from(cardsList).forEach(function(e) {
+	e.addEventListener("click", functionsCards.onDelBtnClicked);
+});
+*/
+left.addEventListener("click", function(e) {
+	// e.target is the clicked element!
+	// if it was an item with class name btn-card-del
+	if (e.target && e.target.className == "btn-card-del") {
+		// button element found, display console message and carry out remove function
+		console.log("del button was clicked");
+		functionsCards.removeCurrCard();
+	}
+});
