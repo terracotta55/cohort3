b03a0a021e971f736ca9bc4d5bd53a8caeb29abb
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-20 14:09:36 -0600

    added before and after functions. checked ok

--- src/02-dom/scripts/cards.js
+++ src/02-dom/scripts/cards.js
@@ -1,57 +1,88 @@
 let left = document.getElementById("left");
-let leftClass = document.getElementsByClassName("card-parent");
+let cardIndex = 0;
+/*
 let cardsList = document.getElementsByClassName("btn-card-del");
-let cardIndex = 1;
+let leftClass = document.getElementsByClassName("card-parent");
 const origCard = document.getElementsByClassName("card")[0];
+*/
 
 const functionsCards = {
-	addNewCard: e => {
-		// let origCard = e.target.parentNode.parentNode.childNodes[3];
+	addNewCard: () => {
+		const div = document.createElement("div");
+		div.className = "card-one";
+		div.id = "box-one";
+
+		let p = document.createElement("p");
+		p.className = "card-text-content";
+		p.textContent = `Card ${cardIndex}`;
+		div.appendChild(p);
+
+		let buttonBefore = document.createElement("button");
+		buttonBefore.className = "btn-card-before";
+		buttonBefore.textContent = "Add Card Before";
+		div.appendChild(buttonBefore);
+
+		let buttonAfter = document.createElement("button");
+		buttonAfter.className = "btn-card-after";
+		buttonAfter.textContent = "Add Card After";
+		div.appendChild(buttonAfter);
+
+		let buttonDel = document.createElement("button");
+		buttonDel.className = "btn-card-del";
+		buttonDel.textContent = "Delete Card";
+		div.appendChild(buttonDel);
+
+		console.log(div);
+
+		return div;
+		/*
+		let origCard = e.target.parentNode.parentNode.childNodes[3];
 		let dupCard = origCard.cloneNode(true);
 		console.log(e.target.parentNode.parentNode.childNodes[3]);
 		console.log(dupCard);
 		left.appendChild(dupCard);
-		// change id and text content after appending clone
 		document.getElementsByClassName("card")[cardIndex].setAttribute("id", `box-${cardIndex}`);
 		document.getElementsByClassName("card-text-content")[cardIndex].textContent = `Card-${cardIndex}`;
 		document.getElementsByClassName("btn-card-before")[cardIndex].setAttribute("id", `btn-before-${cardIndex}`);
 		document.getElementsByClassName("btn-card-after")[cardIndex].setAttribute("id", `btn-after-${cardIndex}`);
 		document.getElementsByClassName("btn-card-del")[cardIndex].setAttribute("id", `btn-del-${cardIndex}`);
 		cardIndex++;
-
-		return true;
+		*/
 	},
-	onAddCardClicked: e => {
-		console.log("add clicked");
-		functionsCards.addNewCard(e);
+	onAddCardClicked: () => {
+		cardIndex++;
+		console.log("event listener: add");
+		left.appendChild(functionsCards.addNewCard());
 	},
 	removeCurrCard: e => {
 		// e.target is the clicked element
 		if (e.target.tagName == "BUTTON") {
-			let btnId = e.target.getAttribute("id");
-			console.log("removeCurrCard called:", btnId);
+			let btnClass = e.target.getAttribute("class");
+			console.log("removeCurrCard function:", btnClass);
 			e.target.parentNode.remove(); //adding another parentNode will delete id:left
 		}
-		/*
-			let currCard = document.getElementsByClassName("card")[0];
-			currCard.remove(e.target.parent);
-		*/
-		// let currCard = document.getElementById(btnId);
-		// document.getElementById("box-1").removeChild(currCard);
-		// currCard.remove();
-		// e.parentNode.removeChild(e.parentNode);
-		// document.getElementById("box1").remove(e.target.id == btnId);
-		// this.parentNode.remove();
-		// let origCard = document.getElementsByClassName("card")[0];
-		// origCard.parentNode.removeChild(origCard);
 		return true;
 	},
 	addBeforeCard: e => {
-		console.log("addBeforeCard called:");
+		if (e.target.tagName == "BUTTON") {
+			cardIndex++;
+			let btnClass = e.target.getAttribute("class");
+			let currCard = e.target.parentNode; // parentnode of the button i.e the card
+			let beforeCard = functionsCards.addNewCard();
+			console.log("addBeforeCard function:", btnClass);
+			left.insertBefore(beforeCard, currCard);
+		}
 		return true;
 	},
 	addAfterCard: e => {
-		console.log("addAfterCard called:");
+		if (e.target.tagName == "BUTTON") {
+			cardIndex++;
+			let btnClass = e.target.getAttribute("class");
+			let nextCard = e.target.parentNode.nextSibling; // sibling of the parent (uncle/aunt)
+			let cardToBeAdded = functionsCards.addNewCard();
+			console.log("addAfterCard function:", btnClass);
+			left.insertBefore(cardToBeAdded, nextCard);
+		}
 		return true;
 	}
 };
