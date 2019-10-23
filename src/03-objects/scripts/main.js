18edebd3f85bb788e79b34a84b0bfbf3f08f69aa
Dalton Okechukwu <Dalton@dalton-mac-air.lan>
2019-10-22 20:59:52 -0600

    starting comp-130a accounts app

--- src/03-objects/scripts/main.js
+++ src/03-objects/scripts/main.js
@@ -1,6 +1,8 @@
 import * as functions from "./functions.js";
-import * as accounts from "./accounts.js";
-import { myAccount } from "./accounts.js";
+import * as accounts from "./account.js";
+import { myAccount } from "./account.js";
 
 functions.helloWorld();
 accounts.helloAccounts();
+console.table(myAccount);
+console.log(`your account balance is ${myAccount.balance()}`);
