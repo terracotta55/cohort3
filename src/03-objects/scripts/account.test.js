d70a12c2d6f766de952aed39c19f3c4ac3e0e36d
Dalton Okechukwu <chike911@yahoo.com>
2019-10-29 15:36:26 -0600

    added 6 more tests for accountconttroller methods

--- src/03-objects/scripts/account.test.js
+++ src/03-objects/scripts/account.test.js
@@ -1,4 +1,12 @@
 import { Account, AccountController } from "./account.js";
+let accountData = [
+	{ accountName: "checking account 1", accountBalance: 150 },
+	{ accountName: "checking account 2", accountBalance: 200 },
+	{ accountName: "checking account 3", accountBalance: 900 },
+	{ accountName: "checking account 4", accountBalance: 115 },
+	{ accountName: "checking account 5", accountBalance: 500 }
+];
+const myAccount = new AccountController("dalton", accountData);
 
 test("testing display value formatter", () => {
 	expect(Account.formatDisplayValue("1.997658979375")).toBe(2);
@@ -23,7 +31,35 @@ test("testing throws insufficient funds error", () => {
 	expect(insufficientFunds).toThrowError(new Error("Insufficient Funds!"));
 });
 
-test("testing account controller", () => {
-	let daltonAccounts = new AccountController("savings", []);
-	expect(daltonAccounts.addAccount("savings", 500)).toEqual({ accountBalance: 500, accountName: "savings" }); // check this test, needs work!
+test("testing add account in account controller", () => {
+	expect(myAccount.addAccount("checking account 6", 135)).toEqual({
+		accountName: "checking account 6",
+		accountBalance: 135
+	});
+});
+
+test("testing total balance in account controller", () => {
+	expect(myAccount.totalBalance()).toEqual(2000);
+});
+
+test("testing highest balance in account controller", () => {
+	expect(myAccount.highestBalance()).toEqual(900);
+});
+
+test("testing lowest balance in account controller", () => {
+	expect(myAccount.lowestBalance()).toEqual(115);
+});
+
+test("testing remove account in account controller", () => {
+	expect(myAccount.removeAccount("checking account 2")).toEqual([
+		{ accountName: "checking account 1", accountBalance: 150 },
+		{ accountName: "checking account 3", accountBalance: 900 },
+		{ accountName: "checking account 4", accountBalance: 115 },
+		{ accountName: "checking account 5", accountBalance: 500 },
+		{ accountName: "checking account 6", accountBalance: 135 }
+	]);
+});
+
+test("testing number of accounts in account controller", () => {
+	expect(myAccount.accountsList.length).toEqual(5);
 });
