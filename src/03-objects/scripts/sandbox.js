9e0e31b7a34e4a12b667c94898f6238e3e3044b8
Dalton Okechukwu <chike911@yahoo.com>
2019-11-03 10:17:31 -0700

    testing out sample async functions and server response

--- src/03-objects/scripts/sandbox.js
+++ src/03-objects/scripts/sandbox.js
@@ -253,12 +253,94 @@ const getFirstName = data => {
 };
 
 const getAllFirstNames = data => {
-	const nameArr = [];
-	data.forEach(element => {
-		nameArr.push(element.name);
-	});
-	return nameArr;
+	return data.map(arrElem => arrElem.name);
 };
 
 console.log(getFirstName(data));
 console.log(getAllFirstNames(data));
+/*
+function showDelayProblem() {
+	console.log("One");
+	setTimeout(() => {
+		// Simulates a fetch
+		console.log("Two");
+	}, 1 * 2000);
+	console.log("Three"); // We have a problem Huston
+}
+showDelayProblem();
+*/
+/*
+async function showDelaySolution() {
+	try {
+		console.log("One");
+		const value = await // Simulate fetch
+		new Promise((resolve, reject) => setTimeout(() => resolve("Two"), 1 * 3000));
+		// Now that we have the value we can use it.
+		console.log(value);
+		console.log("Three");
+	} catch (error) {
+		console.log(error);
+	}
+}
+console.log("Before: showDelaySolution");
+showDelaySolution();
+console.log("After: showDelaySolution");
+*/
+
+const asyncFunctions = {
+	url: "https://uinames.com/api/?amount=10",
+	getFirstName(data) {
+		return data[0].name;
+	},
+	getAllFirstNames(data) {
+		return data.map(arrElem => arrElem.name);
+	},
+	showDelayProblem() {
+		console.log("One");
+		setTimeout(() => {
+			console.log("Two");
+		}, 1 * 2000); // Simulates a fetch
+		console.log("Three"); // We have a problem, this executes despite the timeout
+	},
+	async showDelaySolution() {
+		try {
+			console.log("One");
+			const value = await // Simulate fetch
+			new Promise((resolve, reject) => setTimeout(() => resolve("Two"), 1 * 2000));
+			console.log(value); // Now that we have the value we can use it.
+			console.log("Three"); // This only runs after promise is fulfilled
+		} catch (error) {
+			console.log(error);
+		}
+	},
+
+	async getUsers() {
+		try {
+			const response = await fetch(asyncFunctions.url);
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			console.error("Error:", error);
+			throw error;
+		}
+	},
+
+	async workWithData() {
+		const data = await asyncFunctions.getUsers();
+		console.log(asyncFunctions.getFirstName(data));
+		console.log(asyncFunctions.getAllFirstNames(data));
+	}
+};
+// asyncFunctions.showDelayProblem();
+// asyncFunctions.showDelaySolution();
+// asyncFunctions.getUsers();
+asyncFunctions.workWithData();
+const me = {
+	name: "Dalton",
+	surname: "Okechukwu",
+	gender: "male",
+	region: "Canada"
+};
+const stringifyMe = JSON.stringify(me);
+console.log(me);
+console.log(stringifyMe);
