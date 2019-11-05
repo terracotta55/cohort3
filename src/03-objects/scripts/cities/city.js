75200e12d3f7321ec0e16256a71c76d6ab2708b1
Dalton Okechukwu <chike911@yahoo.com>
2019-11-04 23:19:45 -0700

    added postData function for server fetch

--- src/03-objects/scripts/cities/city.js
+++ src/03-objects/scripts/cities/city.js
@@ -1,6 +1,33 @@
+import { cityCards } from "./cityfunctions.js";
+
+const url = "http://localhost:5000/";
+
+export async function postData(url = "", data = {}) {
+	// Default options are marked with *
+	const response = await fetch(url, {
+		method: "POST", // *GET, POST, PUT, DELETE, etc.
+		mode: "cors", // no-cors, *cors, same-origin
+		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: "same-origin", // include, *same-origin, omit
+		headers: {
+			"Content-Type": "application/json"
+			// 'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		redirect: "follow", // manual, *follow, error
+		referrer: "no-referrer", // no-referrer, *client
+		body: JSON.stringify(data) // body data type must match "Content-Type" header
+	});
+	const json = await response.json(); // parses JSON response into native JavaScript objects
+	// console.log(response.status)
+	// console.log(response.statusText)
+	// console.log(response);
+	// console.log(json);
+	return json;
+}
+
 export class City {
-	constructor(cityKey, cityName, cityLatitude, cityLongitude, cityPopulation) {
-		this.cityKey = cityKey; //should be unique identifier for each city
+	constructor(key, cityName, cityLatitude, cityLongitude, cityPopulation) {
+		this.key = key; //should be unique identifier for each city
 		this.cityName = cityName;
 		this.cityLatitude = cityLatitude;
 		this.cityLongitude = cityLongitude;
@@ -10,13 +37,17 @@ export class City {
 		return `
 		Name: ${this.cityName}\nLatitude: ${this.cityLatitude}\nLongitude: ${this.cityLongitude}\nPopulation: ${this.cityPopulation}`.trim();
 	}
-	async movedIn(num) {
+	movedIn(num) {
 		this.cityPopulation += num;
 		console.log(this.cityPopulation);
+		console.log(this);
+		postData(url + "update", this);
 	}
-	async movedOut(num) {
+	movedOut(num) {
 		this.cityPopulation -= num;
 		console.log(this.cityPopulation);
+		console.log(this);
+		postData(url + "update", this);
 	}
 	howBig() {
 		if (this.cityPopulation >= 1 && this.cityPopulation <= 100) {
@@ -41,6 +72,28 @@ export class Community {
 	constructor() {
 		this.cityNamesArr = [];
 	}
+	getLastKey() {
+		fetch(url + "all")
+			.then(request => request.json())
+			.then(data => {
+				const lastKey = data.reduce((acc, cityObj) => {
+					// console.log(acc, cityObj.key);
+					return acc > cityObj.key ? acc : cityObj.key;
+				}, 1);
+				console.log(lastKey);
+				return lastKey;
+			});
+	}
+	getAllCities() {
+		fetch(url + "all")
+			.then(request => request.json())
+			.then(data => {
+				data.map(city => {
+					this.createCity(Number(city.key), city.cityName, Number(city.cityLatitude), Number(city.cityLongitude), Number(city.cityPopulation));
+					cityCards.createCardDiv();
+				});
+			});
+	}
 	whichSphere(cityObj) {
 		if (cityObj.cityLatitude < 0) return `Southern Hemisphere`;
 		if (cityObj.cityLatitude > 0) return `Northern Hemisphere`;
@@ -57,12 +110,15 @@ export class Community {
 	getTotalPopulation() {
 		return this.cityNamesArr.reduce((accumulator, city) => city.cityPopulation + accumulator, 0);
 	}
-	createCity(cityKey, cityName, cityLatitude, cityLongitude, cityPopulation) {
-		let newCity = new City(cityKey, cityName, cityLatitude, cityLongitude, cityPopulation);
+	createCity(key, cityName, cityLatitude, cityLongitude, cityPopulation) {
+		let newCity = new City(key, cityName, cityLatitude, cityLongitude, cityPopulation);
 		this.cityNamesArr.push(newCity);
+		// console.log(this);
+		postData(url + "add", newCity);
 		return newCity;
 	}
-	async deleteCity(cityKeyToBeRemoved) {
-		this.cityNamesArr = this.cityNamesArr.filter(city => city.cityKey !== cityKeyToBeRemoved);
+	async deleteCity(key) {
+		this.cityNamesArr = this.cityNamesArr.filter(city => city.cityKey !== keyToBeRemoved);
+		postData(url + "delete", { key });
 	}
 }
