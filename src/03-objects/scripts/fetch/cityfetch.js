d4f56c5d68d7a10010f149d5b5b7bd5631ae661a
Dalton Okechukwu <chike911@yahoo.com>
2019-11-07 11:59:51 -0700

    imported city class and refactored methods

--- src/03-objects/scripts/fetch/cityfetch.js
+++ src/03-objects/scripts/fetch/cityfetch.js
@@ -1,5 +1,5 @@
 const url = "http://127.0.0.1:5000/";
-
+import { City, Community } from "../cities/city.js";
 export const cityFetch = {
 	async postData(url = "", data = {}) {
 		const response = await fetch(url, {
@@ -17,7 +17,7 @@ export const cityFetch = {
 		const json = await response.json();
 		return json;
 	},
-	async pageLoad(newCommunity) {
+	async getAllCitiesServer(newCommunity) {
 		let data = await cityFetch.postData(url + "all");
 		if (data.length != 0) {
 			newCommunity.cityNamesArr = data.map(itm => new City(itm.key, itm.cityName, itm.cityLatitude, itm.cityLongitude, itm.cityPopulation));
@@ -33,7 +33,7 @@ export const cityFetch = {
 		let data = await this.postData(url + "add", newCity);
 		return data;
 	},
-	async clearListServer() {
+	async clearServerOnLoad() {
 		let data = await this.postData(url + "clear");
 	},
 	async updateCityServer(currentCity) {
