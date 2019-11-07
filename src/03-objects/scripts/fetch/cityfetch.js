c624f5fe4011b7025ee2d33092869e66bc22d764
Dalton Okechukwu <chike911@yahoo.com>
2019-11-06 23:09:49 -0700

    new file, fetch fuctions for server communication

---
+++ src/03-objects/scripts/fetch/cityfetch.js
@@ -0,0 +1,45 @@
+const url = "http://127.0.0.1:5000/";
+
+export const cityFetch = {
+	async postData(url = "", data = {}) {
+		const response = await fetch(url, {
+			method: "POST",
+			mode: "cors",
+			cache: "no-cache",
+			credentials: "same-origin",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			redirect: "follow",
+			referrer: "no-referrer",
+			body: JSON.stringify(data)
+		});
+		const json = await response.json();
+		return json;
+	},
+	async pageLoad(newCommunity) {
+		let data = await cityFetch.postData(url + "all");
+		if (data.length != 0) {
+			newCommunity.cityNamesArr = data.map(itm => new City(itm.key, itm.cityName, itm.cityLatitude, itm.cityLongitude, itm.cityPopulation));
+			let keysArray = newCommunity.cityNamesArr.map(itm => itm.key);
+			keysArray.sort((a, b) => b - a);
+			let lastKey = keysArray[0];
+			return lastKey;
+		}
+		let lastKey = 0;
+		return lastKey;
+	},
+	async postNewToServer(newCity) {
+		let data = await this.postData(url + "add", newCity);
+		return data;
+	},
+	async clearListServer() {
+		let data = await this.postData(url + "clear");
+	},
+	async updateCityServer(currentCity) {
+		let data = await this.postData(url + "update", currentCity);
+	},
+	async deleteCityServer(removeKey) {
+		let data = await this.postData(url + "delete", { key: removeKey });
+	}
+};
