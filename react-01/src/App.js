bad41508d8b5850d7af28305bd17605f056853ff
Dalton Okechukwu <chike911@yahoo.com>
2019-11-14 13:25:48 -0700

    added state, counter and button event

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>I am developing this application and my name is Dalton</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.counter = 21;
+    this.state = {
+      myState: "No State"
+    };
+  }
+  onPushMe = () => {
+    console.log("button pushed");
+    this.setState({ myState: `New State: ${this.counter++}` });
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            Hi I'm Dalton, and I'm developing this app!
+            {" " + this.state.myState}
+          </h1>
+          <button onClick={this.onPushMe}>Push Me</button>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
