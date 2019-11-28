98311af7ba526f7561873581ed938e92dd6e208d
Dalton Okechukwu <chike911@yahoo.com>
2019-11-28 16:03:05 -0700

    passed props to inputbox component

--- react-02/src/components/account/Account.js
+++ react-02/src/components/account/Account.js
@@ -1,13 +1,38 @@
 import React, { Fragment } from "react";
 import "./Account.css";
+// import InputGroup from "react-bootstrap/InputGroup";
+// import FormControl from "react-bootstrap/FormControl";
+import InputBox from "./InputBox";
 
 class Account extends React.Component {
   constructor() {
     super();
     this.state = {
-      pageStatus: `Account Controller`
+      account: {
+        name: "",
+        balance: 0
+      }
     };
+    this.onAccNameChange = this.onAccNameChange.bind(this);
+    this.onAccBalChange = this.onAccBalChange.bind(this);
   }
+  onAccNameChange(e) {
+    console.log("acc name change");
+    this.setState({
+      account: {
+        name: e.target.value
+      }
+    });
+  }
+  onAccBalChange(e) {
+    console.log("acc bal change");
+    this.setState({
+      account: {
+        balance: e.target.value
+      }
+    });
+  }
+
   render() {
     return (
       <Fragment>
@@ -20,14 +45,12 @@ class Account extends React.Component {
           </div>
           <div id="right">
             <br />
-            <form action="#">
-              <label>Account Name:&emsp;</label>
-              <input type="text" id="inputName" value="" />
-              <br />
-              <label>Initial Balance:&emsp;</label>
-              <input type="text" id="inputBalance" value="" />
-              <br />
-            </form>
+            <label>Account Name:</label>
+            <InputBox onChange={this.onAccNameChange} />
+            <br />
+            <label>Initial Balance:</label>
+            <InputBox onChange={this.onAccBalChange} />
+            <br />
             <br />
             <div id="right-btns">
               <button id="btn-right-add">Add Account</button> <br />
@@ -37,10 +60,10 @@ class Account extends React.Component {
               <button id="btn-right-length">Number of Accounts</button> <br />
             </div>
             <br />
-            <p class="results-header">
+            <p className="results-header">
               <strong>Results will display here:</strong>
             </p>
-            <p class="para-right-card-output"></p>
+            <p className="para-right-card-output"></p>
           </div>
         </div>
       </Fragment>
