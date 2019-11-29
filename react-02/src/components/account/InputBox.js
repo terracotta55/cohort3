d506c221bc40bfb3ac47d3565d2c5c3d5198c69e
Dalton Okechukwu <chike911@yahoo.com>
2019-11-29 14:18:22 -0700

    deleted inputbox, added class component

--- react-02/src/components/account/InputBox.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function InputBox(props) {
-  return <input name="accInput" id="accInput" onChange={props.onChange} />;
-}
-
-export default InputBox;
---
+++ react-02/src/components/account/InputForm.js
@@ -0,0 +1,62 @@
+import React, { Fragment } from "react";
+import "./Account.css";
+import InputBtn from "./InputBtn";
+
+class InputForm extends React.Component {
+  constructor() {
+    super();
+    this.countStart = 0;
+    this.state = {
+      counter: 0,
+      accountName: "",
+      accountBalance: 0
+    };
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.handleSubmitForm = this.handleSubmitForm.bind(this);
+  }
+  handleFormChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+  handleSubmitForm(e) {
+    e.preventDefault(e);
+    this.setState(prevState => {
+      return { counter: prevState.counter + 1 };
+    });
+    this.props.onSubmit(this.state);
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h2>
+          <strong>Enter Account Details</strong>
+        </h2>
+        <form onSubmit={this.handleSubmitForm}>
+          <label>Account Name:</label>
+          <input
+            onChange={this.handleFormChange}
+            name="accountName"
+            type="text"
+            id="accInput"
+          />
+          <label>Initial Balance:</label>
+          <input
+            onChange={this.handleFormChange}
+            name="accountBalance"
+            type="number"
+            id="accInput"
+            min="0"
+            step="0.01"
+          />
+          <InputBtn />
+        </form>
+      </Fragment>
+    );
+  }
+}
+
+export default InputForm;
