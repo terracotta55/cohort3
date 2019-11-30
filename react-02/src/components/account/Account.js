cdeeec47ca9971a59ff964aa56c332df802bf3ca
Dalton Okechukwu <chike911@yahoo.com>
2019-11-29 22:11:36 -0700

    sorted displaycards render and setstate for accountsarray

--- react-02/src/components/account/Account.js
+++ react-02/src/components/account/Account.js
@@ -3,6 +3,7 @@ import "./Account.css";
 import InputForm from "./InputForm";
 import ResultsDisp from "./ResultsDisp";
 import { AccountController } from "./Controller";
+import Cards from "./Cards";
 
 class Account extends React.Component {
   constructor() {
@@ -13,20 +14,39 @@ class Account extends React.Component {
     this.account = new AccountController();
   }
 
-  addReactAccount(params) {
+  addReactAccount = params => {
     const { counter, accountName, accountBalance } = params;
     this.account.addAccount(counter, accountName, accountBalance);
-  }
+    console.log(this.account.accountNamesArr);
+    this.setState({
+      accountsArray: this.account.accountNamesArr
+    });
+    console.log(this.state.accountsArray);
+  };
+
+  displayCards = () => {
+    return this.state.accountsArray.map(account => {
+      return (
+        <Cards
+          key={account.counter}
+          name={account.accountName}
+          balance={account.accountBalance}
+        />
+      );
+    });
+  };
 
   render() {
+    const card = this.displayCards();
+    console.log(card);
     return (
       <Fragment>
         <div id="container">
           <div id="left">
             <h2>
-              <strong>Accounts Controller</strong>
+              <strong>Account Cards</strong>
             </h2>
-            <div id="leftChild"></div>
+            <div id="leftChild">{card}</div>
           </div>
           <div id="right">
             <InputForm onSubmit={this.addReactAccount} />
