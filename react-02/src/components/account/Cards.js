9d454b40376e21b922f12d61806a41009f130991
Dalton Okechukwu <chike911@yahoo.com>
2019-12-01 11:55:33 -0700

    display multiple cards with appropriate user feedback

--- react-02/src/components/account/Cards.js
+++ react-02/src/components/account/Cards.js
@@ -12,6 +12,31 @@ class Cards extends React.Component {
   handleInputChange(e) {
     this.setState({ cardInput: e.target.value });
   }
+  formatCardInput(userInputInCard) {
+    if (
+      isNaN(userInputInCard) ||
+      typeof userInputInCard === "undefined" ||
+      userInputInCard === ""
+    ) {
+      return false;
+    }
+    return parseFloat(userInputInCard);
+  }
+  handleDepositBtn = () => {
+    let cardInputValue = this.formatCardInput(this.state.cardInput);
+    console.log("deposit clicked", cardInputValue);
+    if (!cardInputValue || cardInputValue <= 0) {
+      this.setState({
+        cardResult: `Enter Valid Deposit Amount`
+      });
+      return;
+    }
+    this.props.depositCard(cardInputValue);
+    this.setState({
+      cardInput: "",
+      cardResult: `Deposited $${cardInputValue.toFixed(2)}`
+    });
+  };
   render() {
     let { accountName, accountBalance } = this.props.accountArr;
     return (
@@ -20,13 +45,17 @@ class Cards extends React.Component {
           <span className="para-left-card-name">{accountName}</span>
           <br />
           <input
+            value={this.state.cardInput}
+            onChange={this.handleInputChange}
             className="card-input"
             type="number"
             min="0"
             step="0.01"
-          />{" "}
+          />
           <br />
-          <button className="btn-card-dep">Deposit</button>
+          <button className="btn-card-dep" onClick={this.handleDepositBtn}>
+            Deposit
+          </button>
           <button className="btn-card-wth">Withdraw</button>
           <button className="btn-card-bal">Balance</button>
           <br />
@@ -35,6 +64,7 @@ class Cards extends React.Component {
           <span className="para-left-card-output">
             Balance: {accountBalance}
           </span>
+          <span className="para-left-card-output">{this.state.cardResult}</span>
         </div>
       </Fragment>
     );
