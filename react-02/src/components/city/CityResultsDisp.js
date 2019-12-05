16e3a8a13d943a71788c89c9463c3b12fd432aae
Dalton Okechukwu <chike911@yahoo.com>
2019-12-04 21:15:25 -0700

    receiving appropriate  props for screen display

--- react-02/src/components/city/CityResultsDisp.js
+++ react-02/src/components/city/CityResultsDisp.js
@@ -4,16 +4,16 @@ function ResultsDisp(props) {
   return (
     <div id="resultsContainer">
       <label className="results-label">Total Population:</label>
-      <span className="results-span">{props.totalBal}</span>
+      <span className="results-span">{props.totalPopulation}</span>
       <br />
       <label className="results-label">Most Northern:</label>
-      <span className="results-span">{props.highestBal}</span>
+      <span className="results-span">{props.northmostLatitude}</span>
       <br />
       <label className="results-label">Most Southern:</label>
-      <span className="results-span">{props.lowestBal}</span>
+      <span className="results-span">{props.southmostLatitude}</span>
       <br />
       <label className="results-label">Additional Info:</label>
-      <span className="results-span">{props.numberAcc}</span>
+      <span className="results-span">{props.numberOfCities}</span>
     </div>
   );
 }
