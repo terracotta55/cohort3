a326d96d9cf098cc75aab6abe7015a5898734217
Dalton Okechukwu <chike911@yahoo.com>
2019-12-21 09:13:30 -0700

    deleted files

--- react-02/src/components/hooks/LinkedList.test.js
+++
@@ -1,8 +0,0 @@
-import { ListNode, LinkedList } from "./LinkedListFns.js";
-
-test("show details of subject and amount", () => {
-  let newNode = new ListNode("oneAccount", 100);
-  expect(newNode.showDetails()).toEqual(
-    `subject: oneAccount\namount: 100`.trim()
-  );
-});
