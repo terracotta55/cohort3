dbe3ef0a6dd90adb19c4fae015b13a930dd81d1f
Dalton Okechukwu <chike911@yahoo.com>
2019-12-29 01:06:23 -0700

    :lock: completed module 04

--- src/python/tutorial/04_first_python_app/code.py
+++ src/python/tutorial/04_first_python_app/code.py
@@ -2,4 +2,6 @@ user_age = input("Enter your age: ")
 age_number = int(user_age)
 
 months = age_number * 12
-print(f"{age_number} is equal to {months} months.")
+days = age_number * 12 * 365
+print(f"{age_number} years is equal to {months} months.")
+print(f"{age_number} years is equal to {days} days.")
