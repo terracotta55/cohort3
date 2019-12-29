4f5a47398e48fa40d69531ef9dca2466ca13abff
Dalton Okechukwu <chike911@yahoo.com>
2019-12-29 00:58:28 -0700

    :lock: completed module 02

--- src/python/tutorial/02_string_formatting/code.py
+++ src/python/tutorial/02_string_formatting/code.py
@@ -1,27 +1,21 @@
 name = "Bob"
 greeting = "Hello, Bob"
-
 print(greeting)
 
 name = "Rolf"
-
 print(greeting)
 
 greeting = f"Hello, {name}"
 print(greeting)
 
-# --
-
+# -- Usign format string --
 name = "Anne"
-print(
-    greeting
-)  # This still prints "Hello, Rolf" because `greeting` was calculated earlier.
-print(
-    f"Hello, {name}"
-)  # This is correct, since it uses `name` at the current point in time.
+# This still prints "Hello, Rolf" because `greeting` was calculated earlier.
+print(greeting)
+# This is correct, since it uses `name` at the current point in time.
+print(f"Hello, {name}")
 
 # -- Using .format() --
-
 # We can define template strings and then replace parts of it with another value, instead of doing it directly in the string.
 
 greeting = "Hello, {}"
