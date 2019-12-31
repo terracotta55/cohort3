8b85110f8a45337bf9ffbc4d5ed239d28a5f1ef5
Dalton Okechukwu <chike911@yahoo.com>
2019-12-31 14:52:56 -0700

    :lock: completed module 17

--- src/python/tutorial/17_default_parameter_values/code.py
+++ src/python/tutorial/17_default_parameter_values/code.py
@@ -3,27 +3,27 @@ def add(x, y=3):
 
 
 add(5)  # 8
-add(5, 8)  # 13
+add(5, 10)  # 15
+'''
 add(y=3)  # Error, missing x
-
+'''
 # -- Order of default parameters --
 
 # def add(x=5, y):  # Not OK, default parameters must go after non-default
 #     print(x + y)
 
-# -- Usually don't use variables as default value --
-
+# -- Don't use variables as default value! --
 default_y = 3
 
 
-def add(x, y=default_y):
+def add_two_numbers(x, y=default_y):
     sum = x + y
     print(sum)
 
 
-add(2)  # 5
+add_two_numbers(10)  # 13
 
-default_y = 4
-print(default_y)  # 4
+default_y = 5
+print(default_y)  # 5
 
-add(2)  # 5, even though we re-defined default_y
+add(10)  # 13, even though we re-defined default_y
