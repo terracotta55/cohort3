ee01f9b5190187a17ff2389ad801a635a232da6c
Dalton Okechukwu <chike911@yahoo.com>
2020-01-06 15:38:21 -0700

    added new function for number of rows

--- src/python/read_data.py
+++ src/python/read_data.py
@@ -1,22 +1,35 @@
 import csv
 
+file = 'Community_Census_2018.csv'
 
-def read_data_count():
-    with open('Community_Census_2018.csv', 'r') as csv_file:
+
+def read_data_sum():
+    with open(file, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         total = 0
 
         for row in csv_reader:
             total = sum(float(row["RES_CNT"]) for row in csv_reader)
-            print(total)
+            return total
 
 
-read_data_count()
+print('Total Calgary Population:\t', read_data_sum())
+print("======================================================")
+
+
+def read_data_count_rows():
+    number_rows = 0
+    with open(file) as csv_file:
+        number_rows += sum(1 for line in csv_file)
+        return number_rows - 1  # compensate for initialization
+
+
+print('Number of rows:\t', read_data_count_rows())
 print("======================================================")
 
 
 def read_data_list_all():
-    with open('Community_Census_2018.csv', 'r') as csv_file:
+    with open(file, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             print(row['NAME'], row['CLASS'], row['CLASS_CODE'],
@@ -28,7 +41,7 @@ print("======================================================")
 
 
 def read_data_list_select():
-    with open('Community_Census_2018.csv', 'r') as csv_file:
+    with open(file, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             for i in row['NAME'].split():
