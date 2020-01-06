18216fbbaf8b0e7d034aca85b0a4c2cdac5e7175
Dalton Okechukwu <chike911@yahoo.com>
2020-01-05 17:37:30 -0700

    additional functions for selective neighborhood data listing

--- src/python/read_data.py
+++ src/python/read_data.py
@@ -1,14 +1,41 @@
 import csv
 
 
-def read_data_census():
+def read_data_count():
     with open('Community_Census_2018.csv', 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
+        total = 0
 
         for row in csv_reader:
-            print(row['NAME'], row['CLASS'], row['SECTOR'], row['RES_CNT'])
+            total = sum(float(row["RES_CNT"]) for row in csv_reader)
+            print(total)
 
 
-read_data_census()
-# total = sum(float(row["RES_CNT"]) for row in csv_reader)
-# print(total)
+read_data_count()
+print("======================================================")
+
+
+def read_data_list_all():
+    with open('Community_Census_2018.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            print(row['NAME'], row['CLASS'], row['CLASS_CODE'],
+                  row['SECTOR'], row['RES_CNT'])
+
+
+read_data_list_all()
+print("======================================================")
+
+
+def read_data_list_select():
+    with open('Community_Census_2018.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            for i in row['NAME'].split():
+                if i.startswith("E"):
+                    print(row['NAME'], row['CLASS'], row['CLASS_CODE'],
+                          row['SECTOR'], row['RES_CNT'])
+
+
+read_data_list_select()
+print("======================================================")
