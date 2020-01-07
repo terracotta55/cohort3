1efe40d679f1ba25e3f2ac607dc6b29fb1a18ff5
Dalton Okechukwu <chike911@yahoo.com>
2020-01-06 22:46:40 -0700

    comp-220: completed data read/write exercise with output file

--- src/python/read_data.py
+++ src/python/read_data.py
@@ -1,6 +1,7 @@
 import csv
+from collections import defaultdict
 
-file = 'Community_Census_2018.csv'
+file = 'census_data.csv'
 
 
 def read_data_sum():
@@ -13,10 +14,6 @@ def read_data_sum():
             return total
 
 
-print('Total Calgary Population:\t', read_data_sum())
-print("======================================================")
-
-
 def read_data_count_rows():
     number_rows = 0
     with open(file) as csv_file:
@@ -24,8 +21,42 @@ def read_data_count_rows():
         return number_rows - 1  # compensate for initialization
 
 
-print('Number of rows:\t', read_data_count_rows())
-print("======================================================")
+total_pop = read_data_sum()
+total_rows = read_data_count_rows()
+
+
+def read_data_group():
+    with open(file, 'r') as csv_file:
+        census_data = csv.DictReader(csv_file)
+        by_class = defaultdict(int)
+        # Residential,Industrial,Major Park,Residual Sub Area
+        by_sector = defaultdict(int)
+        # CENTRE,SOUTH,NORTHEAST,NORTHWEST,NORTH,SOUTHEAST,EAST,WEST
+        '''
+        A defaultdict(int) allows you to use keys that aren't defined yet and automatically assigns a value of 0 to them. So you can just do cities["Bolton"] += 10 and it will either create a new key "Bolton" with a value of 10 or add 10 to the value if the key already exists. If you did that with a normal dict, you'd get lots of KeyErrors.
+        '''
+        for row in census_data:
+            by_class[row["CLASS"]] += int(row["RES_CNT"])
+            by_sector[row["SECTOR"]] += int(row["RES_CNT"])
+        with open('census_report.txt', 'w') as out_file:
+            output_writer = csv.writer(out_file)
+            output_writer.writerow(["CLASS\t\tRES_CNT"])
+            output_writer.writerow(["======================================"])
+            output_writer.writerows([f"{one_class}\t{by_class[one_class]}"]
+                                    for one_class in by_class)
+            output_writer.writerow("")
+            output_writer.writerow(["SECTOR\t\tRES_CNT"])
+            output_writer.writerow(["======================================"])
+            output_writer.writerows([f"{one_sector}\t{by_sector[one_sector]}"]
+                                    for one_sector in by_sector)
+            output_writer.writerow(["======================================"])
+            output_writer.writerow([f"Total Population:\t {total_pop}"])
+            output_writer.writerow(["======================================"])
+            output_writer.writerow([f"Total Rows:\t {total_rows}"])
+            output_writer.writerow(["======================================"])
+
+
+read_data_group()
 
 
 def read_data_list_all():
@@ -36,8 +67,7 @@ def read_data_list_all():
                   row['SECTOR'], row['RES_CNT'])
 
 
-read_data_list_all()
-print("======================================================")
+# read_data_list_all()
 
 
 def read_data_list_select():
@@ -50,5 +80,4 @@ def read_data_list_select():
                           row['SECTOR'], row['RES_CNT'])
 
 
-read_data_list_select()
-print("======================================================")
+# read_data_list_select()
