277794a64f34ac8225a99ba3edc024fa1597b034
Dalton Okechukwu <chike911@yahoo.com>
2020-01-07 15:56:33 -0700

    initial commit comp-230

--- src/python/invoice_data.py
+++ src/python/invoice_data.py
@@ -1,9 +1,1 @@
 from openpyxl import Workbook
-
-new_workbook = Workbook()
-sheet_1 = new_workbook.active
-
-sheet_1["A1"] = "write into"
-sheet_1["B1"] = "excel worksheet"
-
-new_workbook.save(filename="hello_world.xlsx")
