40dab1c5f51a0281638fd04d97c552f3d5bf824d
Dalton Okechukwu <chike911@yahoo.com>
2020-02-02 21:43:06 -0700

    functions for invoice maker

---
+++ src/python/comp-230/invoice_functions.py
@@ -0,0 +1,31 @@
+def sheet_to_dict(sheet):
+    header = sheet[1]
+    newDict = {}
+    for row in sheet.rows:
+        rowDict = {}
+        index = 0
+        for cell in row:
+            rowDict[header[index].value] = cell.value
+            index = index+1
+        newDict[row[0].value] = rowDict
+    newDict.pop(sheet['A2'])
+    return newDict
+
+
+def make_invoice(invoice_id, invoicesDict, customersDict, invoice_linesDict, productsDict):
+    invoice = invoicesDict[invoice_id]
+    customer = customersDict[invoice['customer_id']]
+    items = []
+    for key in invoice_linesDict:
+        if invoice_linesDict[key]['invoice_id'] == invoice_id:
+            items.append(invoice_linesDict[key])
+            product = productsDict[items[-1]['product_id']]
+            items[-1] = {
+                'id': product['id'],
+                'description': product['description'],
+                'weight': product['weight'],
+                'unit': product['unit'],
+                'quantity': items[-1]['quantity'],
+                'unit_price': items[-1]['unit_price']
+            }
+    return {'invoice': invoice, 'customer': customer, 'items': items}
